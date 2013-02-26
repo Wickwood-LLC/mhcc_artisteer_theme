423fe0bccf0bb7737704e34e6b00224e1b6fe42c
iMac20SteveW <webmaster@wickwoodonline.com>
2013-02-25 20:24:58 -0500

    Formated code with BBEdit

--- mhcc_2/node.tpl.php
+++ mhcc_2/node.tpl.php
@@ -8,22 +8,25 @@ die();
 }
 $is_blog_page = isset($node->body['und'][0]['summary']) && ($node->body['und'][0]['summary'] == 'ART_BLOG_PAGE') ? true : false;
 ?>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-	<?php if (!$is_blog_page): ?>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"
+<?php print $attributes; ?>
+> 
+<?php if (!$is_blog_page): ?>
 <article class="art-post art-article">
-                                <div class="art-postmetadataheader">
-                                        <?php print render($title_prefix); ?>
+	<div class="art-postmetadataheader">
+<?php print render($title_prefix); ?>
 <?php echo art_node_title_output($title, $node_url, $page); ?>
 <?php print render($title_suffix); ?>
-
-                                    </div>
-                                                <?php if ($submitted): ?>
-<div class="art-postheadericons art-metadata-icons"><?php echo art_submitted_worker($date, $name); ?>
-</div><?php endif; ?>
-
-                <div class="art-postcontent art-postcontent-0 clearfix"><div class="art-article">
-  <?php endif; ?>
-  <?php
+	</div>
+<?php if ($submitted): ?>
+	<div class="art-postheadericons art-metadata-icons">
+<?php echo art_submitted_worker($date, $name); ?>
+	</div>
+<?php endif; ?>
+	<div class="art-postcontent art-postcontent-0 clearfix">
+		<div class="art-article">
+<?php endif; ?>
+<?php
     // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
     hide($content['links']);
@@ -32,20 +35,21 @@ $is_blog_page = isset($node->body['und'][0]['summary']) && ($node->body['und'][0
     print $user_picture;
     print render($content);
   ?>
-  <?php if (!$is_blog_page): ?>
-</div>
-</div>
-                                <?php $access_links = true;
+<?php if (!$is_blog_page): ?>
+		</div>
+	</div>
+<?php $access_links = true;
 if (isset($content['links']['#access'])) {
 $access_links = $content['links']['#access'];
 }
 if ($access_links && (isset($content['links']) || isset($content['comments']))):
 $output = art_links_woker_D7($content);
 if (!empty($output)):	?>
-<div class="art-postfootericons art-metadata-icons"><?php echo $output; ?>
-</div><?php endif; endif; ?>
-
-                
-
-<?php print render($content['comments']); ?></article>	<?php endif; ?>
+	<div class="art-postfootericons art-metadata-icons">
+<?php echo $output; ?>
+	</div>
+<?php endif; endif; ?>
+<?php print render($content['comments']); ?>
+</article>
+<?php endif; ?>
 </div>
